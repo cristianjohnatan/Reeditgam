905217f0f10ca78c4b786e94c39aa4a150923dfc
Cristian Gonzalez <acconsentitodipatate@gmail.com>
2015-04-23 01:54:30 -0500

    04. Agregar nuevos post staticos

--- SITE/scripts/app.js
+++ SITE/scripts/app.js
@@ -12,4 +12,11 @@ modulo1.controller("mainCtrl",[
 		 {title: "Post 4", upvotes: 9},
 		 {title: "Post 5", upvotes: 3},
 		 {title: "Post 6", upvotes: 5}];
+
+		 // Metodo del controlador
++		 $scope.addPost = function(){
++		 	$scope.posts.push(
++		 		{title:"Nuevo Post",
++		 		 upvotes: 0});
++		 };
 	}]);
