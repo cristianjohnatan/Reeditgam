8d975fffc2257924ecc7918a4b79cdabbca11326
Cristian Gonzalez <acconsentitodipatate@gmail.com>
2015-04-23 01:49:09 -0500

    02. Post con ng-repeat

--- SITE/scripts/app.js
+++ SITE/scripts/app.js
@@ -1,8 +1,15 @@
-console.log("#IvanR: Cargo app.js");
+console.log("#CristianGonzalez: Cargo app.js");
 var modulo1 = 
 	angular.module("reeditgam",[]);
 modulo1.controller("mainCtrl",[
 	'$scope',
 	function($scope){
 		$scope.test = "Hola Angular";
+		$scope.posts = 
+		["Post 1 Hola",
+		 "Post 2",
+		 "Post 3",
+		 "Post 4",
+		 "Post 5",
+		 "Post 6"];
 	}]);
